268f1d3967578ad6c4003ccce2122c0303d219a3
Caio Rodrigues <103125100+DevDorf@users.noreply.github.com>
2022-09-21 20:25:33 -0300

    Update C#

--- Curso Nanodegree/PrimerioAppConsole/PrimerioAppConsole/Program.cs
+++ Curso Nanodegree/PrimerioAppConsole/PrimerioAppConsole/Program.cs
@@ -3,6 +3,10 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello Word!");
+        Console.WriteLine("Meu nome é Caio");
+        Console.WriteLine("Meu e-mail é: caiorodrigue1989@gmai.com");
+        Console.WriteLine("55+ (11) 94240-4406");
+
         Console.ReadLine();
     }
 }
