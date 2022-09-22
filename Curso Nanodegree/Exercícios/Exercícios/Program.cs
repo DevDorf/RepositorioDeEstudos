3c40f3187fbc0bd372ec86cfe6f3d90d316d5dae
Caio Rodrigues <103125100+DevDorf@users.noreply.github.com>
2022-09-22 19:12:50 -0300

    Update C#

---
+++ Curso Nanodegree/Exercícios/Exercícios/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Digite o valor do produto: ");
+decimal valor = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Digite a quantidade desejada:");
+int quantidade = Convert.ToInt32(Console.ReadLine());
+
+decimal resultado = quantidade * valor;
+
+Console.WriteLine("O valor total da compra é: {0:c}", resultado);
+
+Console.WriteLine("Qual o numero de parcelas?");
+int numParcela = Convert.ToInt32(Console.ReadLine());
+
+decimal parcela = resultado / numParcela;
+
+Console.Write("Você tera uma parcela de: {0:c}", parcela);
+
+
+
