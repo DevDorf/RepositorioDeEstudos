5bbedeac030567b7e9983bebd2cacfc6190408b7
Caio Rodrigues <103125100+DevDorf@users.noreply.github.com>
2022-09-22 17:14:19 -0300

    Update C#

--- Curso Nanodegree/PrimerioAppConsole/PrimerioAppConsole/Program.cs
+++ Curso Nanodegree/PrimerioAppConsole/PrimerioAppConsole/Program.cs
@@ -7,6 +7,26 @@
         Console.WriteLine("Meu e-mail é: caiorodrigue1989@gmai.com");
         Console.WriteLine("55+ (11) 94240-4406");
 
+        string nome;
+        int idade;
+
+        Console.WriteLine("Digite o se nome:");
+        nome = Console.ReadLine();
+
+        Console.WriteLine("Digite a sua idade:");
+        idade = Convert.ToInt32(Console.ReadLine());
+
+        int dias = idade * 365;
+
+        Console.WriteLine("Olá {0}, você já viveu {1} dias.", nome, dias);
         Console.ReadLine();
+
+
+
+
+
+
+
+        
     }
 }
