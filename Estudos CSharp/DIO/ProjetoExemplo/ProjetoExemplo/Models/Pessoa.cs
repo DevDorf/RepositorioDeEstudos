a3154743845e4fde2f5266a7469c782a9598efd7
Caio Rodrigues <103125100+DevDorf@users.noreply.github.com>
2022-09-23 22:01:58 -0300

    Update C#

---
+++ Estudos CSharp/DIO/ProjetoExemplo/ProjetoExemplo/Models/Pessoa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoExemplo.Models
+{
+    internal class Pessoa
+    {
+        public string name { get; set; }
+        public int idade { get; set; }
+
+        public void Apresenta()
+        {
+            Console.WriteLine($"Meu nome é {nome} e tenho {idade} anos");
+
+        }
+        
+    }
+}
