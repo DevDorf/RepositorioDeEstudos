901da36db4809c6d749234617509a08bfefba1fb
Caio Rodrigues <103125100+DevDorf@users.noreply.github.com>
2022-09-26 20:00:37 -0300

    update C#
    Exercicios beecrowd

---
+++ Estudos CSharp/Beecrowd/ExercicíosDeLógica/ExercicíosDeLógica/Program.cs
@@ -0,0 +1,61 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//beecrowd - 1000 - Hello World!
+//
+//Console.WriteLine("Hello World!");
+
+
+//beecrowd | 1001 - Extremamente Básico
+//
+//int A = Convert.ToInt32(Console.ReadLine());
+//int B = Convert.ToInt32(Console.ReadLine());
+//
+//int X = A + B;
+//
+//Console.WriteLine($"X = {X}");
+
+
+//beecrowd | 1002 - Área do Círculo
+//
+//double n = 3.14159;
+//
+//double raio = Convert.ToDouble(Console.ReadLine());
+//
+//double areaCircunferencia = n * Math.Pow(raio, 2);
+//
+//Console.WriteLine($"A={areaCircunferencia.ToString("N4")}");
+
+
+//beecrowd | 1003 - Soma Simples
+//
+//int A = Convert.ToInt32(Console.ReadLine());
+//int B = Convert.ToInt32(Console.ReadLine());
+//
+//int soma = A + B;
+//
+//Console.WriteLine($"SOMA = {soma}");
+
+
+//beecrowd | 1004 - Produto Simples
+//
+//int A = Convert.ToInt32(Console.ReadLine());
+//int B = Convert.ToInt32(Console.ReadLine());
+//
+//int produto = A * B;
+//
+//Console.WriteLine($"PROD = {produto}");
+
+
+//beecrowd | 1005 - Média 1
+
+double a = Convert.ToDouble(Console.ReadLine());
+double b = Convert.ToDouble(Console.ReadLine());
+
+double media = ((3.5 * a) + (7.5 * b)) / 11;
+
+Console.WriteLine($"MEDIA = {Math.Round(media, 1)}");
+
+
+
+
+
--- Estudos CSharp/DIO/ProjetoExemplo/ProjetoExemplo/Program.cs
+++ Estudos CSharp/DIO/ProjetoExemplo/ProjetoExemplo/Program.cs
@@ -1,22 +1,28 @@
 ﻿using ProjetoExemplo.Models;
 
-string apresentacao = "Olá, tudo bem?";
-
-int quantidade = 1;
-
-double altura = 1.80;
-
-decimal preco = 1.80M;
-
-bool condicao = true;
-
-Console.WriteLine(apresentacao);
-Console.WriteLine("Valor da variável quantidade: " + quantidade);
-Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00"));
-Console.WriteLine("Valor da variável preço: " + preco);
-Console.WriteLine("Valor da variável condição: " + condicao);
+DateTime dataAtual = DateTime.Now.AddDays(5); // adiciona dias a data tual.
+Console.WriteLine(dataAtual.ToString("dd/mm/yyyy"));
 
 
+//string apresentacao = "Olá, tudo bem?";
+
+//int quantidade = 1;
+//quantidade = 10;
+
+
+
+//double altura = 1.80;
+
+//decimal preco = 1.80M;
+
+//bool condicao = true;
+
+
+//Console.WriteLine(apresentacao);
+//Console.WriteLine("Valor da variável quantidade: " + quantidade);
+//Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00"));
+//Console.WriteLine("Valor da variável preço: " + preco);
+//Console.WriteLine("Valor da variável condição: " + condicao);
 
 
 
