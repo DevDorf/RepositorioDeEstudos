e2e548c97bda97c56923be7358cf5d3b48626911
Caio Rodrigues <103125100+DevDorf@users.noreply.github.com>
2022-10-03 19:52:42 -0300

    Update C#

--- Estudos CSharp/DIO/Fundamentos/Program.cs
+++ Estudos CSharp/DIO/Fundamentos/Program.cs
@@ -1,7 +1,43 @@
 ﻿using Fundamentos.Models;
 
-Pessoa pessoa1 = new Pessoa();
 
-pessoa1.nome = "Caio";
-pessoa1.idade = 33;
-pessoa1.Apresentar();
+int a = 10;
+int b = 20;
+
+int c = a + b;
+
+Console.WriteLine(c);
+
+
+
+
+
+// DateTime dataAtual = DateTime.Now.AddDays(5);
+// Console.WriteLine(dataAtual.ToString("dd/MM/yyyy HH:mm"));
+
+
+// string apresentacao = "Olá, seja bem vindo";
+
+// int quantidade = 1;
+// quantidade = 10;
+
+// double altura = 1.80;
+
+// decimal preco = 1.80M;
+
+// bool condicao = true;
+
+// Console.WriteLine(apresentacao);
+// Console.WriteLine("Valo da variavel quantidade: " + quantidade);
+// Console.WriteLine("Valor da variavel altura: " + altura.ToString("0.00"));
+// Console.WriteLine("Valor da variavel preço: " + preco);
+// Console.WriteLine("Valor da variavel condição:" + condicao);
+
+
+
+
+// Pessoa pessoa1 = new Pessoa();
+
+// pessoa1.nome = "Caio";
+// pessoa1.idade = 33;
+// pessoa1.Apresentar();
