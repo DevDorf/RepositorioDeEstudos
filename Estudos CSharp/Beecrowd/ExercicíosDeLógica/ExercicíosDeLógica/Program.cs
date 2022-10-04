2c4209ead4d1a7a40f7896f0b89605db079ab3cc
Caio Rodrigues <103125100+DevDorf@users.noreply.github.com>
2022-10-03 23:07:01 -0300

    Update C#

--- Estudos CSharp/Beecrowd/ExercicíosDeLógica/ExercicíosDeLógica/Program.cs
+++ Estudos CSharp/Beecrowd/ExercicíosDeLógica/ExercicíosDeLógica/Program.cs
@@ -55,22 +55,61 @@
 
 // Console.WriteLine("MEDIA = " + media);
 
-//beecrowd | 1006 - Média 2
 
-double a = Convert.ToDouble(Console.ReadLine());
-double b = Convert.ToDouble(Console.ReadLine());
-double c = Convert.ToDouble(Console.ReadLine());
+// //beecrowd | 1006 - Média 2
 
-double media = ((a * 2) + (b * 3) + (c * 5))/ 10;
+// double a = Convert.ToDouble(Console.ReadLine());
+// double b = Convert.ToDouble(Console.ReadLine());
+// double c = Convert.ToDouble(Console.ReadLine());
 
-Console.WriteLine("MEDIA = ") + media;
+// double media = ((a * 2) + (b * 3) + (c * 5))/ 10;
+
+// Console.WriteLine("MEDIA = ") + media;
+
+
+// //beecrowd | 1007 - Diferença
+
+// int a = Convert.ToInt32(Console.ReadLine());
+// int b = Convert.ToInt32(Console.ReadLine());
+// int c = Convert.ToInt32(Console.ReadLine());
+// int d = Convert.ToInt32(Console.ReadLine());
+
+// int diferenca = (a * b - c * d);
+
+// Console.WriteLine($"DIFERENCA = {diferenca}");
+
+
+
+// //beecrowd | 1008 - Salário
+
+// int funcionarios = Convert.ToInt32(Console.ReadLine());
+// int horasTrabalhadas = Convert.ToInt32(Console.ReadLine());
+// decimal valorRecebeHora = Convert.ToDecimal(Console.ReadLine());
+
+// decimal salario = horasTrabalhadas * valorRecebeHora;
+
+// Console.WriteLine($"NUMBER = {funcionarios}");
+// Console.WriteLine($"SALARY = U$ {salario}");
+
+
+
+//beecrowd | 1009 - Salário com Bônus
+
+string nome = Console.ReadLine();
+decimal salarioFixo = Convert.ToDecimal(Console.ReadLine());
+decimal totalVendas = Convert.ToDecimal(Console.ReadLine());
+
+decimal bonus = totalVendas * (15/100);
+
+decimal salarioFinal = salarioFixo + bonus;
+
+Console.WriteLine($"TOTAL = R$ {salarioFixo}");
 
 
 
 
-//beecrowd | 1006
-//beecrowd | 1006
-//beecrowd | 1006
+
+
 //beecrowd | 1006
 //beecrowd | 1006
 //beecrowd | 1006
