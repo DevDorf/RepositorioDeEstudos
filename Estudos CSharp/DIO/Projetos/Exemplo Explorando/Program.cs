a3abea767a8790083df8a1a853a0883b6d877bb2
Caio Rodrigues <103125100+DevDorf@users.noreply.github.com>
2023-01-17 18:43:06 -0300

    Metodos, propriedades e construtores

---
+++ Estudos CSharp/DIO/Projetos/Exemplo Explorando/Program.cs
@@ -0,0 +1,32 @@
+﻿using Exemplo_Explorando.Models;
+
+Pessoa p1 = new Pessoa(nome: "Caio", sobrenome: "Rodrigues");
+Pessoa p2 = new Pessoa(nome: "Tuã", sobrenome: "Souza");
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Ingles";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Pessoa p1 = new Pessoa();
+// p1.Nome = "Caio";
+// p1.Sobrenome = "Rodrigues";
+// p1.Idade = 33;
+// p1.Apresentar();
