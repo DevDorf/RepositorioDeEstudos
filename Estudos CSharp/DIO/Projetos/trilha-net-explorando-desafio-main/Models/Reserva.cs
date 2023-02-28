7766ba51c821dec66f62f527f810adfb621e9626
Caio Rodrigues <103125100+DevDorf@users.noreply.github.com>
2023-02-28 01:30:47 -0300

    Update desafio Explorando dio

---
+++ Estudos CSharp/DIO/Projetos/trilha-net-explorando-desafio-main/Models/Reserva.cs
@@ -0,0 +1,59 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
+            
+            if (hospedes.Count < Suite.Capacidade)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
+                throw new Exception("O numero total de hospedes ultrapassa a capacidade de ocupação da suite");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
+            int quantidadeDeHospedes = Hospedes.Count;
+            return quantidadeDeHospedes;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            // TODO: Retorna o valor da diária
+            decimal valorTotal = DiasReservados * Suite.ValorDiaria;
+            decimal valor = valorTotal;
+            decimal desconto = valorTotal * (10 / 100);
+
+            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
+            if (DiasReservados >= 10)
+            {
+                valor = valorTotal - desconto;
+            }
+
+            return valor;
+        }
+    }
+}
