edf8d686f10d846a237835a3c1a50298ee6d2a5d
Caio Rodrigues <103125100+DevDorf@users.noreply.github.com>
2023-03-01 23:50:50 -0300

    Update desafio POO

--- Estudos CSharp/DIO/Projetos/trilha-net-poo-desafio-main/Program.cs
+++ Estudos CSharp/DIO/Projetos/trilha-net-poo-desafio-main/Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone NOKIA: ");
+Smartphone nokia = new Nokia(numero: "1234567",modelo: "Modelo 1", imei: "111111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+System.Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone IPHONE: ");
+Smartphone iphone = new Iphone(numero: "1234567",modelo: "Modelo 1", imei: "111111111", memoria: 64);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
